424e603c2f29168475ea9acdab7b2bd9d784657b
kuangtu <kuangtuweb@gmail.com>
2019-10-20 10:28:46 +0800

    dataframe check null

--- dataframe/checknull.py
+++ dataframe/checknull.py
@@ -25,5 +25,41 @@ def checknull():
     #list each columns missing data sum
     print(df.isnull().sum())
 
+def countnan():
+    '''
+    统计多个Nan数目
+    :return:
+    '''
+
+    # 创建一个dataframe
+    students = [('jack', np.NaN, 'Sydeny', 'Australia'),
+                ('Riti', np.NaN, 'Delhi', 'India'),
+                ('Vikas', 31, np.NaN, 'India'),
+                ('Neelu', 32, 'Bangalore', 'India'),
+                ('John', 16, 'New York', 'US'),
+                ('John', 11, np.NaN, np.NaN),
+                (np.NaN, np.NaN, np.NaN, np.NaN)
+                ]
+    # Create a DataFrame object
+    dfObj = pd.DataFrame(students, columns=['Name', 'Age', 'City', 'Country'])
+    print(dfObj)
+
+    # 统计df中Nan是否有null，单元格中返回False或者True
+    # 只包含了True或者False
+    print(dfObj.isnull())
+
+    # 按照列进行检查
+    print(dfObj.isnull().sum())
+
+    # 统计整个的df
+    print(dfObj.isnull().sum().sum())
+
+    # 统计每行的缺失
+    for i in range(len(dfObj.index)):
+        # 通过iloc取出了一行，返回了该行的Series
+        print("Nan in Row", i, ":", dfObj.iloc[i].isnull().sum())
+
 if __name__ == '__main__':
-    checknull()
+    # checknull()
+
+    countnan()
